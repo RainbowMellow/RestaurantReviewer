1ec6ce55e5b697aae8c86f0631a281acff172bbe
rizvan99 <rizvan2312@hotmail.com>
2021-05-06 10:39:55 +0200

    Revert "mapsactivity ready to receive and send intents to other views"
    This reverts commit b4d8e767e5a34ef927cd33d956a4d836c98cf778.

--- app/src/main/java/com/example/restaurantreviewer/GUI/MainActivity.kt
+++ app/src/main/java/com/example/restaurantreviewer/GUI/MainActivity.kt
@@ -1,11 +1,7 @@
 package com.example.restaurantreviewer.GUI
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.Toast
 import com.example.restaurantreviewer.R
 
 class MainActivity : AppCompatActivity() {
@@ -13,20 +9,4 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            R.id.menu_map -> {
-                val intentMap = Intent(this@MainActivity, MapsActivity::class.java)
-                startActivity(intentMap)
-            }
-        }
-        return super.onOptionsItemSelected(item)
-    }
-
 }
